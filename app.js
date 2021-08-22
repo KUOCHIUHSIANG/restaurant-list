c6b048e2417cae7cb6018f697fad0a738c4b24dc
Jater <p833795p@gmail.com>
2021-08-22 22:01:11 +0800

    install npm/express and setting project-file-path and setting font-awesome/bootstrap/css/javascript  and render restaurant-list...

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.render('index', {restaurantList: restaurantList.results})
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
