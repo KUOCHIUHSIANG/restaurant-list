21c0df1ef9aec3a6e472102470fd280af991f871
Jater <p833795p@gmail.com>
2021-08-22 22:44:57 +0800

    get search's keyword with querystring and set routes for ''/search''

--- app.js
+++ app.js
@@ -10,12 +10,20 @@ app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
-  res.render('index', {restaurantList: restaurantList.results})
+  res.render('index', { restaurantList: restaurantList.results })
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+   return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurantList: restaurants, keyword: keyword })
 })
 
 app.listen(port, () => {
