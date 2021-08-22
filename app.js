45c33b535578201f72112f5c0727ce6d3cc7f1c2
Jater <p833795p@gmail.com>
2021-08-22 22:29:09 +0800

    render showpage with params

--- app.js
+++ app.js
@@ -13,18 +13,9 @@ app.get('/', (req, res) => {
   res.render('index', {restaurantList: restaurantList.results})
 })
 
-app.get('/restaurants/1', (req, res) => {
-  const restaurantOne = {
-    id: 1,
-    name: "布娜飛比利時啤酒餐廳",
-    category: "義式餐廳",
-    location: "台北市松山區市民大道四段 185 號",
-    google_map: "https://goo.gl/maps/V9mKwVJ4s5v",
-    phone : "02 2570 1255",
-    description: "我們希望帶給您的，不只是啤酒，有美食，還有一份對生活的熱情。 義大利語「Bravo」的原意─「喝采」、「讚揚」， 我想著如果有一個大家都能輕鬆品嚐美酒、享受美食的地方，那就真的是太棒了！ 因為這個念頭，加上一股對比利時啤酒的熱情， 於是「Bravo Beer布娜飛比利時啤酒餐廳」在2006年誕生了...",
-    image : "https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5634/08.jpg",
-  }
-  res.render('show', { restaurant: restaurantOne })
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
 })
 
 app.listen(port, () => {
